8403a30362a5151d9617e2fbf50d56f60a7298b6
André Mossige <mossige.a@gmail.com>
2019-01-31 14:21:36 +0100

    TAG-263: Legg til TODO om config av devServer

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
 const CracoLessPlugin = require("craco-less");
 
 module.exports = {
+    // TODO: Konfigurer devServer som fjerner NAV dekoratør
+    // https://webpack.js.org/configuration/dev-server/
+    // https://github.com/navikt/foreldrepengeplanlegger/blob/master/src/build/webpack/devserver.config.js
     plugins: [{ plugin: CracoLessPlugin }]
 };
