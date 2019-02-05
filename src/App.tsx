25e6bcb57cf9b229ac48c34dc4954560ecca2cc3
André Mossige <mossige.a@gmail.com>
2019-02-05 12:45:20 +0100

    TAG-270 Fjern knapper brukt til å teste loggeren

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import * as React from 'react';
 import './App.less';
-import { logError, logEvent } from './utils/metricsUtils';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <h1>Kontaktskjema</h1>
-                <button
-                    onClick={() => {
-                        logEvent('kontakt-oss.en.test.event');
-                    }}
-                >
-                    Logg event
-                </button>
-                <button
-                    onClick={() => {
-                        logError('En feil har skjedd.');
-                    }}
-                >
-                    Logg til Grafana
-                </button>
             </div>
         );
     }
