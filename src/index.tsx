e681758fb9a4126d76b3849582b774714035986e
André Mossige <mossige.a@gmail.com>
2019-02-06 08:40:31 +0100

    TAG-248 Flytt serviceWorker til /utils

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
-import { unregister } from './serviceWorker';
+import { unregister } from './utils/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
