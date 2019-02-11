06ad579931bc2e9140879f1618137dc15b9962d6
André Mossige <mossige.a@gmail.com>
2019-02-11 09:56:16 +0100

    TAG-248 Bruk en annen Unleash URL som har skikkelige sertifikater

--- src/utils/konstanter.ts
+++ src/utils/konstanter.ts
@@ -1,5 +1,6 @@
 export const VEIVISER_URL =
     'https://tjenester.nav.no/veiviserarbeidsgiver/samleside';
 export const UNLEASH_URL =
-    'https://unleashproxy.nais.oera.no/api/client/features';
+    'https://unleashproxy.prod-sbs.nais.io/api/client/features';
+// 'https://unleashproxy.nais.oera.no/api/client/features';
 export const PILOTFYLKER_TOGGLE_URL = `${UNLEASH_URL}/kontakt-oss.kontaktskjema`;
