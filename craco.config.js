67068cde2d9b4e54d075b5abea37e4a1b5b1ba03
André Mossige <mossige.a@gmail.com>
2019-02-18 13:21:23 +0100

    TAG-277 Serve tomme verdier for logger.js og settings.js lokalt

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,19 @@
-const CracoLessPlugin = require("craco-less");
+const CracoLessPlugin = require('craco-less');
 
 module.exports = {
-    // TODO: Konfigurer devServer som fjerner NAV dekoratør
-    // https://webpack.js.org/configuration/dev-server/
-    // https://github.com/navikt/foreldrepengeplanlegger/blob/master/src/build/webpack/devserver.config.js
-    plugins: [{ plugin: CracoLessPlugin }]
+    devServer: {
+        before: app => {
+            app.get('/frontendlogger/logger.js', (req, res) => {
+                res.json({});
+            });
+
+            app.get('/kontakt-oss/static/js/settings.js', (req, res) => {
+                res.send("window.appSettings = {MILJO: 'lokalt'};");
+            });
+            // TODO: Konfigurer slik at {{{NAV_STYLES}}} ++ fjernes
+            // https://webpack.js.org/configuration/dev-server/
+            // https://github.com/navikt/foreldrepengeplanlegger/blob/master/src/build/webpack/devserver.config.js
+        },
+    },
+    plugins: [{ plugin: CracoLessPlugin }],
 };
