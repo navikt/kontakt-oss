cbdd730ce8315eaa2d4647f5bc668ef7ba1efb84
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-03-13 10:33:51 +0100

    TAG-379 Downgrade node

--- src/KontaktOss/Kontaktskjema/validering.test.ts
+++ src/KontaktOss/Kontaktskjema/validering.test.ts
@@ -1,8 +1,5 @@
 import { orgnrOk } from './validering';
 
-// TODO Det er en issue i Jest som hindrer testene i å kjøre på CircleCI. https://github.com/facebook/create-react-app/issues/6591
-// TODO Det er fikset i Jest, CRA har ikke oppdatert versjonen. Midlertidig fix: Inkluder "jest-util": "^23.4.0". Slettes når CRA er oppdatert.
-
 test('Orgnr kan være undefined', () => {
     expect(orgnrOk(undefined)).toBeTruthy();
 });
