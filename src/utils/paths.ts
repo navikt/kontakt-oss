1cbcfa72f4ef537793e274e337bf9904c5ea51fc
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-05-29 13:17:57 +0200

    TAG-527 Bruk togglesystem fra backend

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,8 +2,7 @@ export const BASE_PATH = '/kontakt-oss';
 export const BEKREFTELSE_PATH = '/bekreftelse';
 export const SEND_KONTAKTSKJEMA_PATH = `${BASE_PATH}/api/meldInteresse`;
 export const FYLKER_OG_KOMMUNER_PATH = `${BASE_PATH}/api/fylkerOgKommuner`;
-
-export const FEATURE_TOGGLE_BASEPATH = `${BASE_PATH}/features`;
+export const FEATURE_TOGGLE_BASEPATH = `${BASE_PATH}/api/feature`;
 
 export const featureTogglePath = (features: string[]): string => {
     const query = features.map(feature => `feature=${feature}`).join('&');
