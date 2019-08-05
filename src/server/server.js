44b975d402305574791476fe5553221c8d772303
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-08-05 13:15:25 +0200

    TAG-703 Server skal feile hvis vi ikke får hentet dekoratøren

--- src/server/server.js
+++ src/server/server.js
@@ -45,5 +45,11 @@ const startServer = html => {
 };
 
 getDecorator()
-    .then(renderApp, error => console.log('Kunne ikke hente dekoratør ', error))
-    .then(startServer, error => console.log('Kunne ikke rendre app ', error));
+    .then(renderApp, error => {
+        console.error('Kunne ikke hente dekoratør ', error);
+        process.exit(1);
+    })
+    .then(startServer, error => {
+        console.error('Kunne ikke rendre app ', error);
+        process.exit(1);
+    });
