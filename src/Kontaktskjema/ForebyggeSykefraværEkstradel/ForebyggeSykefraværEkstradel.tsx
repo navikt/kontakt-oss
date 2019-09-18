51fec233d567546f14ce2ffa32bde16e387acfdf
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-18 10:35:08 +0200

    Legg til del som er ekstra for sykefraævr

---
+++ src/Kontaktskjema/ForebyggeSykefraværEkstradel/ForebyggeSykefraværEkstradel.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+
+export const ForebyggeSykefraværEkstradel = () => {
+    // TODO Legg til mer her
+    return (
+        <AlertStripeInfo className="kontaktskjemaSykefravær__alertstripe typo-normal">
+            Arbeidet med å forebygge sykefravær og sikre godt arbeidsmiljø, er
+            et ansvar som deles mellom arbeidsgiver og tillitsvalgte (eller
+            ansattrepresentanter). NAV Arbeidslivssenter kan bistå i dette
+            arbeidet.
+        </AlertStripeInfo>
+    );
+};
--- src/Kontaktskjema/Kontaktskjema.tsx
+++ src/Kontaktskjema/Kontaktskjema.tsx
@@ -2,20 +2,19 @@ import * as React from 'react';
 import { FunctionComponent } from 'react';
 import { useQueryState } from 'react-router-use-location-state';
 import { Temavalg } from './Temavalg/Temavalg';
-import { getTema, Tema, TemaType } from '../utils/kontaktskjemaApi';
+import { Tema, TemaType } from '../utils/kontaktskjemaApi';
 
 import './kontaktskjema.less';
+import { ForebyggeSykefraværEkstradel } from './ForebyggeSykefraværEkstradel/ForebyggeSykefraværEkstradel';
 
 // TODO TAG-826 Fjern "nytt" i navnet
 const NyttKontaktskjema: FunctionComponent = () => {
-    const [value, setValue] = useQueryState('fylke', '');
-    const [kommune, setKommune] = useQueryState('kommune', '');
-
     const [valgtTemaType, setTemaType] = useQueryState<TemaType | ''>('tema', '');
 
     return (
         <div className="kontaktskjema">
             <Temavalg velgTema={(tema: Tema) => {setTemaType(tema.type)}} valgtTemaType={valgtTemaType}/>
+            {valgtTemaType === TemaType.ForebyggeSykefravær && <ForebyggeSykefraværEkstradel/>}
         </div>
     );
 };
