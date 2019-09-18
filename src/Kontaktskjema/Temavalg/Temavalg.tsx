dd66c92e77992b2c2e4cab8b250f9f90335388e4
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-18 10:30:01 +0200

    Legg til infotekst for temavalg

--- src/Kontaktskjema/Temavalg/Temavalg.tsx
+++ src/Kontaktskjema/Temavalg/Temavalg.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { FunctionComponent } from 'react';
-import { Tema, temaerForNyttKontaktskjema, TemaType } from '../../utils/kontaktskjemaApi';
+import {
+    Tema,
+    temaerForNyttKontaktskjema,
+    TemaType,
+} from '../../utils/kontaktskjemaApi';
 import { logEvent, mapTilTemaEvent } from '../../utils/metricsUtils';
-import { Undertittel } from 'nav-frontend-typografi';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import Temaknapp from './Temaknapp';
 
 import './Temavalg.less';
+import infosirkel from './info-sirkel-fyll.svg';
 
 interface Props {
     velgTema: (tema: Tema) => void;
@@ -19,15 +24,16 @@ export const Temavalg: FunctionComponent<Props> = props => {
     };
 
     const temaknapper = temaerForNyttKontaktskjema.map(tema => {
-        return(
-        <Temaknapp
-            key={tema.type}
-            tema={tema}
-            onClick={onVelgTema}
-            valgt={tema.type === props.valgtTemaType}
-            className="temaknapp"
-        />
-    )});
+        return (
+            <Temaknapp
+                key={tema.type}
+                tema={tema}
+                onClick={onVelgTema}
+                valgt={tema.type === props.valgtTemaType}
+                className="temaknapp"
+            />
+        );
+    });
 
     return (
         <div className="temavalg">
@@ -35,6 +41,16 @@ export const Temavalg: FunctionComponent<Props> = props => {
                 Hva gjelder det?
             </Undertittel>
             <div className="temavalg__wrapper">{temaknapper}</div>
+            <div className="temavalg__infotekst">
+                <img className="temavalg__infotekst-ikon" src={infosirkel} />
+                <Normaltekst>
+                    For andre henvendelser ber vi deg kontakte
+                    arbeidsgivertelefonen på{' '}
+                    <a href={'tel:+4755553336'} className={'lenke'}>
+                        55&nbsp;55&nbsp;33&nbsp;36
+                    </a>
+                </Normaltekst>
+            </div>
         </div>
     );
 };
