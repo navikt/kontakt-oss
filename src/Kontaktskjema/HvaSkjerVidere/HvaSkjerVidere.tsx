fc6ac7adb34daebf7ee3752eb532a7e2b08cc77a
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-20 16:25:40 +0200

    Style steg på desktop

--- src/Kontaktskjema/HvaSkjerVidere/HvaSkjerVidere.tsx
+++ src/Kontaktskjema/HvaSkjerVidere/HvaSkjerVidere.tsx
@@ -3,28 +3,36 @@ import { Undertittel } from 'nav-frontend-typografi';
 import brevSvg from './brev.svg';
 import dameSvg from './nav-dame.svg';
 import skiltSvg from './skilt.svg';
+import pilSvg from './pil.svg';
 import PanelBase from 'nav-frontend-paneler';
 import './HvaSkjerVidere.less';
 import { BildeMedTekst } from './BildeMedTekst/BildeMedTekst';
 
+// TODO Farge på pil-svg?
 export const HvaSkjerVidere: FunctionComponent = () => {
     return (
-        <PanelBase border className="hva-skjer-videre">
-            <Undertittel>
-                Hva skjer med henvendelsen min?
-            </Undertittel>
-            <BildeMedTekst
-                svg={brevSvg}
-                tekst={'Henvendelsen blir sendt til ditt lokale NAV kontor'}
-            />
-            <BildeMedTekst
-                svg={dameSvg}
-                tekst={'Det lokale kontoret ruter den til den på kontoret som best kan svare'}
-            />
-            <BildeMedTekst
-                svg={skiltSvg}
-                tekst={'En NAV-ansatt tar kontakt med deg på telefon eller e-post'}
-            />
+        <PanelBase className="hva-skjer-videre">
+            <Undertittel>Hva skjer med henvendelsen min?</Undertittel>
+            <div className="hva-skjer-videre__steg-wrapper">
+                <BildeMedTekst
+                    svg={brevSvg}
+                    tekst={'Henvendelsen blir sendt til ditt lokale NAV kontor'}
+                />
+                <img src={pilSvg} alt="" className="hva-skjer-videre__pil" />
+                <BildeMedTekst
+                    svg={dameSvg}
+                    tekst={
+                        'Det lokale kontoret ruter den til den på kontoret som best kan svare'
+                    }
+                />
+                <img src={pilSvg} alt="" className="hva-skjer-videre__pil" />
+                <BildeMedTekst
+                    svg={skiltSvg}
+                    tekst={
+                        'En NAV-ansatt tar kontakt med deg på telefon eller e-post'
+                    }
+                />
+            </div>
         </PanelBase>
     );
 };
