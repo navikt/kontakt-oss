96b7d84a8ad2dbc2d8c208c85974fc1ece1e116d
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-10-01 16:33:56 +0200

    Småplukk fra Eivind

--- src/Samleside/AndreKontaktpunkter/ForebyggeSykefravær.tsx
+++ src/Samleside/AndreKontaktpunkter/ForebyggeSykefravær.tsx
@@ -12,15 +12,15 @@ export const ForebyggeSykefravær = () => (
             alt=""
         />
         <Undertittel className="blokk-xs kontaktpunkt-tema__tittel">
-            Forebygging av sykefravær
+            Forebygge sykefravær
         </Undertittel>
         <Normaltekst className="blokk-xs">
-            Ønsker du bistand til forebygging av sykevær eller å hindre frafall
+            Ønsker du hjelp til forebygging av sykevær eller å hindre frafall
             fra arbeidslivet? NAV Arbeidslivssenter samarbeider med virksomheter
             om et mer inkluderende arbeidsliv.
         </Normaltekst>
         <Element className="blokk-xs">Her kan du få svar:</Element>
-        <Link to={KONTAKTSKJEMA_PATH} className="lenke blokk-xs">
+        <Link to={KONTAKTSKJEMA_PATH + '?tema=FOREBYGGE_SYKEFRAVÆR'} className="lenke blokk-xs">
             Send kontaktskjema til NAV Arbeidslivssenter
         </Link>
         <a href={'tel:+4755553336'} className="lenke blokk-xs">
--- src/Samleside/GenerelleHenvendelser/Arbeidsgivertelefonen/Arbeidsgivertelefonen.tsx
+++ src/Samleside/GenerelleHenvendelser/Arbeidsgivertelefonen/Arbeidsgivertelefonen.tsx
@@ -21,7 +21,7 @@ export const Arbeidsgivertelefonen: FunctionComponent<Props> = props => {
                 >
                     55 55 33 36
                 </a>
-                <Normaltekst>Kl. 08:00–15:00 (hverdager)</Normaltekst>
+                <Normaltekst>Kl. 08:00–15:30 (hverdager)</Normaltekst>
             </div>
         </div>
     );
