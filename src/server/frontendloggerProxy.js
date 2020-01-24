7f23b28f63a21b1685e014bdb057b9bdbe5ee914
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-01-24 10:07:08 +0100

    refactor navn

--- src/server/frontendloggerProxy.js
+++ src/server/frontendloggerProxy.js
@@ -1,10 +1,9 @@
 const basePath = require('./basePath');
 const proxy = require('http-proxy-middleware');
 
-const frontendloggerUrlDev = 'https://tjenester-q1.nav.no/frontendlogger';
-const frontendloggerUrlProd = 'https://tjenester.nav.no/frontendlogger';
-
-const frontendloggerUrl = process.env.NAIS_CLUSTER_NAME === 'prod-sbs' ? frontendloggerUrlProd : frontendloggerUrlDev;
+const frontendloggerUrl = process.env.NAIS_CLUSTER_NAME === 'prod-sbs'
+    ? 'https://tjenester-q1.nav.no/frontendlogger'
+    : 'https://tjenester-q1.nav.no/frontendlogger';
 
 const proxyConfig = {
     target: frontendloggerUrl,
