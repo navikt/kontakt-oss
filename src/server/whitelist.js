40d038d86ae95a03e3cd749977bd7e142abdca08
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-01-24 10:14:30 +0100

    .

--- src/server/whitelist.js
+++ src/server/whitelist.js
@@ -3,14 +3,8 @@ const apiBasePath = '/kontakt-oss-api';
 
 const whitelistApiUrler = ['/meldInteresse', '/fylkerOgKommuner'];
 
-const frontendloggerUrlDev = 'https://tjenester-q0.nav.no/frontendlogger';
-const frontendloggerUrlProd = 'https://tjenester.nav.no/frontendlogger';
-
-const frontendloggerUrl = process.env.NAIS_CLUSTER_NAME === 'prod-sbs' ? frontendloggerUrlProd : frontendloggerUrlDev;
-
 const whitelist = {
     ['^' + basePath('/api/feature')]: apiBasePath + '/feature', // går ikke med regexp-en under pga query params
-    ['^' + basePath('/frontendlogger')]: frontendloggerUrl,
 };
 
 whitelistApiUrler.forEach(url => {
