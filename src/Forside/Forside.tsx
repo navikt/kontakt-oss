b0081ed274e755be099aaf931947b1614ca9678b
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-03-16 17:55:15 +0100

    Legg til chatlenke (#58)
    * Legg til lenke til chatting om korona
    
    * Legg til feature toggle på chatlenke

--- src/Forside/Forside.tsx
+++ src/Forside/Forside.tsx
@@ -4,15 +4,18 @@ import { GenerelleHenvendelser } from './GenerelleHenvendelser/GenerelleHenvende
 import './Forside.less';
 import { AndreKontaktpunkter } from './AndreKontaktpunkter/AndreKontaktpunkter';
 import Banner from '../Banner/Banner';
+import Chatlenke from './Chatlenke/Chatlenke';
+import { FeatureToggle, FeatureToggles, medFeatureToggles } from '../providers/FeatureTogglesProvider';
 
-export const Forside: FunctionComponent = () => (
+export const Forside: FunctionComponent<FeatureToggles> = (props) => (
     <>
         <Banner tekst="Kontakt NAV – arbeidsgiver" />
         <div className="forside">
+            {props[FeatureToggle.ChatlenkeFeature] && <Chatlenke />}
             <GenerelleHenvendelser />
             <AndreKontaktpunkter />
         </div>
     </>
 );
 
-export default Forside;
+export default medFeatureToggles(Forside);
