c8bbff01ca7356725a3cacec4cfa3515b53342bf
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-07-03 13:03:40 +0200

    Kjør prettier på server

--- server/decorator.js
+++ server/decorator.js
@@ -5,31 +5,21 @@ const { JSDOM } = jsdom;
 const url =
     'https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true';
 
-const requestDecorator = callback => request(url, callback);
+const requestDecorator = (callback) => request(url, callback);
 
 const getDecorator = () =>
     new Promise((resolve, reject) => {
         const callback = (error, response, body) => {
-            if (
-                !error &&
-                response.statusCode >= 200 &&
-                response.statusCode < 400
-            ) {
+            if (!error && response.statusCode >= 200 && response.statusCode < 400) {
                 const { document } = new JSDOM(body).window;
                 const prop = 'innerHTML';
 
                 const data = {
                     NAV_SCRIPTS: document.getElementById('scripts')[prop],
                     NAV_STYLES: document.getElementById('styles')[prop],
-                    NAV_HEADING: document.getElementById('header-withmenu')[
-                        prop
-                    ],
-                    NAV_FOOTER: document.getElementById('footer-withmenu')[
-                        prop
-                    ],
-                    NAV_MENU_RESOURCES: document.getElementById(
-                        'megamenu-resources'
-                    )[prop],
+                    NAV_HEADING: document.getElementById('header-withmenu')[prop],
+                    NAV_FOOTER: document.getElementById('footer-withmenu')[prop],
+                    NAV_MENU_RESOURCES: document.getElementById('megamenu-resources')[prop],
                 };
                 resolve(data);
             } else {
