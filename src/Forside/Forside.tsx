b5b9ab433366abffaec3227cd1c2f4f06a4d0994
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-08-05 11:15:28 +0200

    Legg til eventer for sidevisninger

--- src/Forside/Forside.tsx
+++ src/Forside/Forside.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import { GenerelleHenvendelser } from './GenerelleHenvendelser/GenerelleHenvendelser';
 import './Forside.less';
 import { AndreKontaktpunkter } from './AndreKontaktpunkter/AndreKontaktpunkter';
 import Banner from '../Banner/Banner';
 import Chatlenke from './Chatlenke/Chatlenke';
 import { FeatureToggles, medFeatureToggles } from '../providers/FeatureTogglesProvider';
+import { sendEvent } from '../amplitude/amplitude';
 
-export const Forside: FunctionComponent<FeatureToggles> = (props) => (
-    <>
-        <Banner tekst="Kontakt NAV – arbeidsgiver" />
-        <div className="forside">
-            <Chatlenke />
-            <GenerelleHenvendelser />
-            <AndreKontaktpunkter />
-        </div>
-    </>
-);
+export const Forside: FunctionComponent<FeatureToggles> = () => {
+    useEffect(() => {
+        sendEvent('forside', 'vist');
+    });
+
+    return (
+        <>
+            <Banner tekst="Kontakt NAV – arbeidsgiver" />
+            <div className="forside">
+                <Chatlenke />
+                <GenerelleHenvendelser />
+                <AndreKontaktpunkter />
+            </div>
+        </>
+    );
+};
 
 export default medFeatureToggles(Forside);
--- src/Fylkesvelger/Fylkesvelger.tsx
+++ src/Fylkesvelger/Fylkesvelger.tsx
@@ -1,31 +1,40 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { fylker } from '../utils/fylker';
 import { Innholdstittel } from 'nav-frontend-typografi';
 
 import './Fylkesvelger.less';
 import Banner from '../Banner/Banner';
 import Lenkepanel from 'nav-frontend-lenkepanel';
+import { scrollToBanner } from '../utils/scrollUtils';
+import { sendEvent } from '../amplitude/amplitude';
 
-const Fylkesvelger: FunctionComponent = () => (
-    <>
-        <Banner tekst="Kontakt NAV – arbeidsgiver" />
-        <div className="fylkesvelger">
-            <Innholdstittel className="fylkesvelger__tittel" tag="h2">
-                Velg fylke
-            </Innholdstittel>
-            {fylker.map((fylke) => (
-                <Lenkepanel
-                    tittelProps="undertittel"
-                    href={fylke.hrefKontaktliste}
-                    className="fylkesvelger__fylke"
-                    border
-                    key={fylke.nokkel}
-                >
-                    {fylke.navn}
-                </Lenkepanel>
-            ))}
-        </div>
-    </>
-);
+const Fylkesvelger: FunctionComponent = () => {
+    useEffect(() => {
+        scrollToBanner();
+        sendEvent('fylkesvelger', 'vist');
+    }, []);
+
+    return (
+        <>
+            <Banner tekst="Kontakt NAV – arbeidsgiver" />
+            <div className="fylkesvelger">
+                <Innholdstittel className="fylkesvelger__tittel" tag="h2">
+                    Velg fylke
+                </Innholdstittel>
+                {fylker.map((fylke) => (
+                    <Lenkepanel
+                        tittelProps="undertittel"
+                        href={fylke.hrefKontaktliste}
+                        className="fylkesvelger__fylke"
+                        border
+                        key={fylke.nokkel}
+                    >
+                        {fylke.navn}
+                    </Lenkepanel>
+                ))}
+            </div>
+        </>
+    );
+};
 
 export default Fylkesvelger;
