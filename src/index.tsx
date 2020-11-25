78d6822d849fa44d57c202565255479f7ad863ff
Malaz Alkoj <malaz.alkoj@nav.no>
2020-11-25 16:17:52 +0100

    legge til sentry med environment sjekk (#80)

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 import { unregister } from './utils/serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: "https://52f2375c2315491592c2ada9202d226e@sentry.gc.nav.no/54",
+    environment: (window as any).appSettings.MILJO,
+    enabled: (window as any).appSettings.MILJO !== 'lokalt',
+});
 
 if (process.env.REACT_APP_MOCK) {
     console.log('========================================');
