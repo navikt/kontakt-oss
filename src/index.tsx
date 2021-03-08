f6834d7c153ca3b7496c2eb9ed1dfae1ab30fe64
Ken Gullaksen <ken.gullaksen@nav.no>
2021-03-08 13:40:52 +0100

    fiks e2e og sentry referanse

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App';
 import { unregister } from './utils/serviceWorker';
 import * as Sentry from '@sentry/browser';
+import './index.less';
+import App from './App';
+import environment from "./utils/environment";
 
 Sentry.init({
     dsn: "https://52f2375c2315491592c2ada9202d226e@sentry.gc.nav.no/54",
-    environment: (window as any).appSettings.MILJO,
-    enabled: (window as any).appSettings.MILJO !== 'lokalt',
+    release: process.env.GIT_COMMIT_HASH || 'unknown',
+    environment: environment.MILJO,
+    enabled: environment.MILJO !== 'local',
 });
 
 if (process.env.REACT_APP_MOCK) {
